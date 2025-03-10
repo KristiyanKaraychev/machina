8249ebefbb1be4500af125de80f8c13eeaf1636e
Kristiyan Karaychev <k.karaychev@gmail.com>
2025-03-10 23:23:14 +0200

    Updated layout and styling of workout items

--- client/src/components/WorkoutListItem.jsx
+++ client/src/components/WorkoutListItem.jsx
@@ -1,12 +1,28 @@
-export default function WorkoutListItem({ workoutName, difficulty, length }) {
+export default function WorkoutListItem({
+    workoutName,
+    difficulty,
+    length,
+    imgURL,
+}) {
     return (
         <>
-            <div className="workout-item">
+            <li className="workout-item">
+                <a href="/">
+                    <h3>{workoutName}</h3>
+                    <img src={imgURL} alt={workoutName}></img>
+                    <p>
+                        {length} min | {difficulty}
+                    </p>
+                </a>
+            </li>
+
+            {/* <a href="/" className="workout-item">
                 <h3>{workoutName}</h3>
+                <img src={imgURL}></img>
                 <p>
                     {length} min | {difficulty}
                 </p>
-            </div>
+            </a> */}
         </>
     );
 }
